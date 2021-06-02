53ac4b93c10190a1a598f6980e350b6215b17d7a
vortex2wenty <baseballmario101@gmail.com>
2021-06-02 04:52:32 -0400

    Add memory cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import ReactSVG from './React.svg';
 import AngularSVG from './Angular.svg';
 import VueSVG from './Vue.svg';
+import jQuerySVG from './jQuery.svg';
+import NodeSVG from './Node.svg';
+import SvelteSVG from './Svelte.svg';
+import BackboneSVG from './Backbone.svg';
+import EmberSVG from './Ember.svg';
+import MeteorSVG from './Meteor.svg';
 import MemoryCard from './components/MemoryCard';
 import uniqid from 'uniqid';
 
@@ -10,6 +16,7 @@ const App = () => {
   let [highScore, setHighScore] = useState(0);
   let [currentScore, setCurrentScore] = useState(0);
   let [cardsClicked, setCardsClicked] = useState([]);
+  let [targetClicked, setTargetClicked] = useState('');
 
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -27,25 +34,41 @@ const App = () => {
     setCardsClicked([]);
   };
 
-  const cardClicked = (e) => {
-    if (cardsClicked.includes(e.target)) {
+  const checkCardValidity = (target) => {
+    if (cardsClicked.includes(target)) {
       gameOver();
     } else {
-      setCardsClicked((prevCardsClicked) => [
-        ...prevCardsClicked,
-        e.target,
-      ]);
+      setCardsClicked((prevCardsClicked) => [...prevCardsClicked, target]);
       setCurrentScore((prevScore) => prevScore + 1);
     }
   };
 
-  let [memoryCards, setMemoryCards] = useState([
+  const cardClicked = (e) => {
+    if (
+      e.target.classList.contains('memory-card-img') ||
+      e.target.classList.contains('memory-card-title')
+    ) {
+      setTargetClicked(e.target.parentElement);
+    } else {
+      setTargetClicked(e.target.parentElement.parentElement);
+    }
+  };
+
+  useEffect(() => {
+    if (targetClicked) {
+      checkCardValidity(targetClicked);
+      setTargetClicked('');
+    }
+  }, [targetClicked]);
+
+  let [memoryCards] = useState([
     <MemoryCard
       img={ReactSVG}
       title="React"
       gameOver={gameOver}
       cardClicked={cardClicked}
       key={uniqid()}
+      dataKey={uniqid()}
     />,
     <MemoryCard
       img={AngularSVG}
@@ -53,6 +76,7 @@ const App = () => {
       gameOver={gameOver}
       cardClicked={cardClicked}
       key={uniqid()}
+      dataKey={uniqid()}
     />,
     <MemoryCard
       img={VueSVG}
@@ -60,13 +84,65 @@ const App = () => {
       gameOver={gameOver}
       cardClicked={cardClicked}
       key={uniqid()}
+      dataKey={uniqid()}
+    />,
+    <MemoryCard
+      img={jQuerySVG}
+      title="jQuery"
+      gameOver={gameOver}
+      cardClicked={cardClicked}
+      key={uniqid()}
+      dataKey={uniqid()}
+    />,
+    <MemoryCard
+      img={NodeSVG}
+      title="Node.js"
+      gameOver={gameOver}
+      cardClicked={cardClicked}
+      key={uniqid()}
+      dataKey={uniqid()}
+    />,
+    <MemoryCard
+      img={SvelteSVG}
+      title="Svelte"
+      gameOver={gameOver}
+      cardClicked={cardClicked}
+      key={uniqid()}
+      dataKey={uniqid()}
+    />,
+    <MemoryCard
+      img={BackboneSVG}
+      title="Backbone.js"
+      gameOver={gameOver}
+      cardClicked={cardClicked}
+      key={uniqid()}
+      dataKey={uniqid()}
+    />,
+<MemoryCard
+      img={EmberSVG}
+      title="Ember.js"
+      gameOver={gameOver}
+      cardClicked={cardClicked}
+      key={uniqid()}
+      dataKey={uniqid()}
+    />,
+<MemoryCard
+      img={MeteorSVG}
+      title="Meteor"
+      gameOver={gameOver}
+      cardClicked={cardClicked}
+      key={uniqid()}
+      dataKey={uniqid()}
     />,
   ]);
 
   return (
     <div className="App">
-      Score: {currentScore}
-      {shuffleArray(memoryCards)}
+      <div className="navbar">
+        <div className="flex-item">Score: {currentScore}</div>
+        <div className="flex-item">High Score: {highScore}</div>
+      </div>
+      <div className="flex-container">{shuffleArray(memoryCards)}</div>
     </div>
   );
 };
--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
-
 const MemoryCard = (props) => {
   return (
-    <div className='memory-card' onClick={props.cardClicked}>
+    <div className='memory-card' onClick={props.cardClicked} data-key={props.dataKey}>
       <div className='memory-card-img'>
         <img src={props.img} alt='' width='100' height='100' />
       </div>
       <div className='memory-card-title'>
-        {props.title}
+        <p>{props.title}</p>
       </div>
     </div>
   );
